d061c27e02df6ae3518c272c5093c8b0a039bc54
mehul <mehul.jain2029@gmail.com>
2016-02-03 17:27:08 +0530

    add comments

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import subprocess
 import time
 import sys
 
+#Popup a notification on ubuntu desktop
 def sendmessage(message):
 	subprocess.Popen(['notify-send',message])
 	return
 
+#Request and parse the given webpage for commentary 
 def get_the_feed():
 	url = urllib.request.urlopen(str(sys.argv[1]))
 	readHtml = url.read();
@@ -16,6 +18,7 @@ def get_the_feed():
 	post = soup.body.find(class_ = "post").text
 	return post 
 
+#Main function 
 previous = ""
 while True:
 	latest = get_the_feed()
